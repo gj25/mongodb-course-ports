26da823077b22374fc5cff3fcf11963496aa147a
Chris Saylor <cjsaylor@gmail.com>
2012-10-24 08:39:30 -0400

    Actually add the simplified hw1-2.js...

--- node/m101/hw1-2.js
+++ node/m101/hw1-2.js
@@ -3,31 +3,22 @@ var _ = require('underscore');
 
 var db = mongodb.Db('m101', new mongodb.Server('localhost', 27017), {safe: true});
 
-db.open(function(err, conn) {
+db.open(function(err, client) {
 	if(err) {
 		console.log('Error opening connection' + err);
 	}
-	conn.collection('funnynumbers', function(err, collection) {
-		var magic = 0;
+	var collection = new mongodb.Collection(client, 'funnynumbers');
+	var magic = 0;
+	collection.find().toArray(function(err, docs) {
 		if (err) {
 			console.log(err);
 		}
-		collection.find(function(err, iterator) {
-			if (err) {
-				console.log(err);
+		_.each(docs, function(iter) {
+			if (iter.value % 3 === 0) {
+				magic += iter.value;
 			}
-			iterator.toArray(function(err, docs) {
-				if (err) {
-					console.log(err);
-				}
-				_.each(docs, function(iter) {
-					if (iter.value % 3 === 0) {
-						magic += iter.value;
-					}
-				});
-				console.log('The answer to Homework One, Problem 2 is ' + magic);
-				conn.close();
-			});
 		});
+		console.log('The answer to Homework One, Problem 2 is ' + magic);
+		client.close();
 	});
 });
