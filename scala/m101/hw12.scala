e5692b1845bf50d3dd352c9ac643afe9de9caa4c
Yura Taras <yura.taras@gmail.com>
2012-10-24 17:36:20 +0300

    using casbah

--- scala/m101/hw12.scala
+++ scala/m101/hw12.scala
@@ -1,35 +1,21 @@
 // @author: Victor Igbokwe
-import com.mongodb._
+import com.mongodb.casbah.Imports._
 
 object hw12 extends App {
-  val mongo = new Mongo
-  val db = mongo.getDB("m101") // create a connection to the database
-  // retrieve/create a reference to the "funnynumbers" collection.
-  val coll = db.getCollection("funnynumbers")
+  val coll = MongoConnection(/*host*/)(/*db*/"m101")(/*collection*/"funnynumbers")
 
-  /*
-   * A recursive function to make a sum of some of the values in the "funnynumbers" collection.
-   * This function is done this way to minimize/eliminate side-effects as Scala encourages.
-   */
-  def traverseCollection(cursor: DBCursor, accumulator: Double): Double = {
-    // a control statement: find out if there is more data to process.
-    if (!cursor.hasNext) accumulator
-    else {
-      // retrieve the next value.
-      val doc = cursor.next
-      // make sure the document has a value called "value".
-      if (doc.containsField("value")) {
-        // convert the retrieved value to a double.
-        val item = doc.get("value").asInstanceOf[Double]
-        traverseCollection(cursor, accumulator + (if (item % 3 == 0) item else 0))
-      } else traverseCollection(cursor, accumulator)
-    }
-  }
+  val entries = coll.find()
 
-  // retrieve the magic value by calling the recursive function
-  // with the result of calling find on the collection.
-  val magic = traverseCollection(coll.find(), 0)
-  // close the database.
-  mongo.close
-  println("The answer to Homework One, Problem 2 is " + magic.asInstanceOf[Int])
+  // TODO I don't know what happens if there are billions of records, hopefully casbah is smart enough to make maps and filter lazy
+  // If it's true, data will be read from data base only during invocation of '.sum'
+  // Otherwise data first will be all retrieved, then mapped, then mapped once more, 
+  // then filtered and only then summed, which may lead to OutOfMemoryError
+  // This is surelly a thing to verify. Anyway, syntax is concise and sexy
+  val sum = entries
+    .map(_.getAs[Double]("value")) // Map cursor to list (hopefully lazy) of Option[Double], where None means record doesn't have value assigned
+    .flatMap(_.toList) // just an idiom to filter out None's and map from list of Some's to list of actual objects
+    .filter(_ % 3 == 0) // filter out numbers which are not funny
+    .sum // well.. just a sum
+
+  println("The answer to Homework One, Problem 2 is " + sum.asInstanceOf[Int])
 }
