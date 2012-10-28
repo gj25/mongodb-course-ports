e13b83b6adae0cbd2acf361b79fa08e863c81d6b
Dmytro Kostiuchenko <edio@archlinux.us>
2012-10-28 15:51:41 +0200

    Performed gofmt

--- golang/src/m101/hw1-2.go
+++ golang/src/m101/hw1-2.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-    "github.com/garyburd/go-mongo/mongo"
-    "fmt"
-    "os"
+	"fmt"
+	"github.com/garyburd/go-mongo/mongo"
+	"os"
 )
 
 func main() {
-    conn,err := mongo.Dial("localhost")
-    if err != nil {
-        fmt.Println("Error trying to connect to database")
-        os.Exit(1)
-    }
+	conn, err := mongo.Dial("localhost")
+	if err != nil {
+		fmt.Println("Error trying to connect to database")
+		os.Exit(1)
+	}
 
-    defer conn.Close()
+	defer conn.Close()
 
-    db := mongo.Database{conn, "m101", mongo.DefaultLastErrorCmd}
+	db := mongo.Database{conn, "m101", mongo.DefaultLastErrorCmd}
 
-    funnynumbers := db.C("funnynumbers")
+	funnynumbers := db.C("funnynumbers")
 
-    cursor,err := funnynumbers.Find(nil).Cursor()
-    if err != nil {
-        fmt.Println("Error trying to read collection")
-        os.Exit(1)
-    }
+	cursor, err := funnynumbers.Find(nil).Cursor()
+	if err != nil {
+		fmt.Println("Error trying to read collection")
+		os.Exit(1)
+	}
 
-    defer cursor.Close()
+	defer cursor.Close()
 
-    var magic int
-    for cursor.HasNext() {
-        var m map[string]interface{}
+	var magic int
+	for cursor.HasNext() {
+		var m map[string]interface{}
 
-        cursor.Next(&m)
+		cursor.Next(&m)
 
-        value := int(m["value"].(float64))
+		value := int(m["value"].(float64))
 
-        if value % 3 == 0 {
-            magic += int(m["value"].(float64))
-        }
+		if value%3 == 0 {
+			magic += int(m["value"].(float64))
+		}
 
-    }
+	}
 
-    fmt.Println("The answer to Homework One, Problem 2 is", magic)
+	fmt.Println("The answer to Homework One, Problem 2 is", magic)
 }
