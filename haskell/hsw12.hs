c0587b0e26eb1bd952b116780ac6af5755f701e5
Yura Taras <yura.taras@gmail.com>
2012-10-30 00:22:12 +0200

    working solution

--- haskell/hsw12.hs
+++ haskell/hsw12.hs
@@ -1,41 +1,22 @@
 {-# LANGUAGE OverloadedStrings, ExtendedDefaultRules #-}
-module Main where
+module Main (main) where
 
 import Database.MongoDB
 import qualified Database.MongoDB as M
 import Control.Monad.Trans (liftIO)
+import Control.Monad (liftM)
 
 main = do
     pipe <- runIOE $ connect (host "127.0.0.1")
-    e <- access pipe master "baseball" run
+    e1 <- access pipe master "m101" runDocs
     close pipe
-    print e
+    print e1
 
-run = do
-    clearTeams
-    insertTeams
-    allTeams >>= printDocs "All Teams"
-    nationalLeagueTeams >>= printDocs "National League Teams"
-    newYorkTeams >>= printDocs "New York Teams"
+runDocs :: Action IO Int
+runDocs = do
+  docs <- find (select [] "funnynumbers")
+  sumValues `liftM` rest docs
 
-
-
-clearTeams = delete (select [] "team")
-
-insertTeams = insertMany "team" [
-    ["name" =: "Yankees", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "American"],
-    ["name" =: "Mets", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "National"],
-    ["name" =: "Phillies", "home" =: ["city" =: "Philadelphia", "state" =: "PA"], "league" =: "National"],
-    ["name" =: "Red Sox", "home" =: ["city" =: "Boston", "state" =: "MA"], "league" =: "American"] ]
-
-allTeams = rest =<< find (select [] "team") {sort = ["home.city" =: 1]}
-
-nationalLeagueTeams = rest =<< find (select ["league" =: "National"] "team")
-
-newYorkTeams = rest =<< find (select ["home.state" =: "NY"] "team") {project = ["name" =: 1, "league" =: 1]}
-
-printDocs title docs = liftIO $ putStrLn title >> mapM_ (print . exclude ["_id"]) docs
-
-sumN :: [Document] -> Int
-sumN d = sum . (filter isMod3) $ d >>= M.lookup "number"
+sumValues :: [Document] -> Int
+sumValues d = sum . (filter isMod3) $ d >>= M.lookup "value"
   where isMod3 x = x `mod` 3 == 0
