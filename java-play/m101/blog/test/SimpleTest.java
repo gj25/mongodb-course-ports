2e3ee2168c94f86a138df98d3a3f1e8c032e9914
gj25 <gjovanovic@novuscom.net>
2012-11-04 14:07:28 -0800

    Blog project with Hw 2.3

--- java-play/m101/blog/test/SimpleTest.java
+++ java-play/m101/blog/test/SimpleTest.java
@@ -2,28 +2,14 @@
 import models.Users;
 import org.junit.Test;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author CAGOJOV
- */
 public class SimpleTest {
     @Test
     public void pwsHashTest()
-    {
-        Users users = new Users();
+    {   
+        System.out.println(" pwd, salt: " + Users.makePwHash("pwd", "salt"));
+        System.out.println(" pwd, salt: " + Users.makePwHash("pwd", "salt"));
+        System.out.println(" pwd, salt: " + Users.makePwHash("pwd", "salt"));
         
-        System.out.println(" pwd, salt 1: " + Users.makePwHash("pwd", "salt"));
-        System.out.println(" pwd, salt 1: " + Users.makePwHash("pwd", "salt"));
-        System.out.println(" pwd, salt 1: " + Users.makePwHash("pwd", "salt"));
-        System.out.println(" pwd, salt 1: " + Users.makePwHash("pwd", "salt"));
-        System.out.println(" pwd, salt 1: " + Users.makePwHash("pwd", "salt"));
-        System.out.println(" pwd1,null 1: " + Users.makePwHash("pwd1", null));
-        
+        System.out.println(" pwd1,null 1: " + Users.makePwHash("pwd1", null));        
     }
-    
 }
